d027206a748ce41ecb1a65fd12c2b52c6730d969
danielbacsur <contact@danielbacsur.com>
2021-11-26 17:56:58 +0100

    store to blog

--- admin_area/insert_blog.php
+++ admin_area/insert_blog.php
@@ -10,7 +10,7 @@ else {
 <div class="col-lg-12" ><!-- col-lg-12 Starts --> 
 <ol class="breadcrumb"><!-- breadcrumb Starts -->
 <li class="active">
-<i class="fa fa-dashboard" ></i> Dashboard / Insert store
+<i class="fa fa-dashboard" ></i> Dashboard / Insert blog
 </li>
 </ol><!-- breadcrumb Ends -->
 </div><!-- col-lg-12 Ends --> 
@@ -20,46 +20,46 @@ else {
 <div class="panel panel-default"><!-- panel panel-default Starts -->
 <div class="panel-heading"><!-- panel-heading Starts -->
 <h3 class="panel-title">
-<i class="fa fa-money fa-fw"></i> Insert store
+<i class="fa fa-money fa-fw"></i> Insert blog
 </h3>
 </div><!-- panel-heading Ends -->
 <div class="panel-body"><!-- panel-body Starts -->
 <form class="form-horizontal" action="" method="post" enctype="multipart/form-data"><!-- form-horizontal Starts -->
 <div class="form-group"><!-- form-group Starts -->
-<label class="col-md-3 control-label"> store Title : </label>
+<label class="col-md-3 control-label"> blog Title : </label>
 <div class="col-md-6">
-<input type="text" name="store_title" class="form-control">
+<input type="text" name="blog_title" class="form-control">
 </div>
 </div><!-- form-group Ends -->
 <div class="form-group"><!-- form-group Starts -->
-<label class="col-md-3 control-label"> store Image : </label>
+<label class="col-md-3 control-label"> blog Image : </label>
 <div class="col-md-6">
-<input type="file" name="store_image" class="form-control">
+<input type="file" name="blog_image" class="form-control">
 </div>
 </div><!-- form-group Ends -->
 <div class="form-group"><!-- form-group Starts -->
-<label class="col-md-3 control-label"> store Description : </label>
+<label class="col-md-3 control-label"> blog Description : </label>
 <div class="col-md-6">
-<textarea name="store_desc" class="form-control" rows="10" cols="19">
+<textarea name="blog_desc" class="form-control" rows="10" cols="19">
 </textarea>
 </div>
 </div><!-- form-group Ends -->
 <div class="form-group"><!-- form-group Starts -->
-<label class="col-md-3 control-label"> store Button : </label>
+<label class="col-md-3 control-label"> blog Button : </label>
 <div class="col-md-6">
-<input type="text" name="store_button" class="form-control">
+<input type="text" name="blog_button" class="form-control">
 </div>
 </div><!-- form-group Ends -->
 <div class="form-group"><!-- form-group Starts -->
-<label class="col-md-3 control-label"> store Url : </label>
+<label class="col-md-3 control-label"> blog Url : </label>
 <div class="col-md-6">
-<input type="url" name="store_url" class="form-control">
+<input type="url" name="blog_url" class="form-control">
 </div>
 </div><!-- form-group Ends -->
 <div class="form-group"><!-- form-group Starts -->
 <label class="col-md-3 control-label"> </label>
 <div class="col-md-6">
-<input type="submit" name="submit" value="Insert store" class="btn btn-primary form-control">
+<input type="submit" name="submit" value="Insert blog" class="btn btn-primary form-control">
 </div>
 </div><!-- form-group Ends -->
 </form><!-- form-horizontal Ends -->
@@ -69,25 +69,25 @@ else {
 </div><!-- 2 row Ends -->
 <?php
 if(isset($_POST['submit'])){
-$store_title = $_POST['store_title'];
-$store_desc = $_POST['store_desc'];
-$store_button = $_POST['store_button'];
-$store_url = $_POST['store_url'];
-$store_image = $_FILES['store_image']['name'];
-$tmp_image = $_FILES['store_image']['tmp_name'];
-$sel_store = "select * from store";
-$run_store = mysqli_query($con,$sel_store);
-$count = mysqli_num_rows($run_store);
+$blog_title = $_POST['blog_title'];
+$blog_desc = $_POST['blog_desc'];
+$blog_button = $_POST['blog_button'];
+$blog_url = $_POST['blog_url'];
+$blog_image = $_FILES['blog_image']['name'];
+$tmp_image = $_FILES['blog_image']['tmp_name'];
+$sel_blog = "select * from blog";
+$run_blog = mysqli_query($con,$sel_blog);
+$count = mysqli_num_rows($run_blog);
 if($count == 3){
-echo "<script>alert('You Have already Inserted 3 store columns')</script>";
+echo "<script>alert('You Have already Inserted 3 blog columns')</script>";
 }
 else{
-move_uploaded_file($tmp_image,"store_images/$store_image");
-$insert_store = "insert into blog (store_title,store_image,store_desc,store_button,store_url) values ('$store_title','$store_image','$store_desc','$store_button','$store_url')";
-$run_store = mysqli_query($con,$insert_store);
-if($run_store){
-echo "<script>alert('New store Column Has Been Inserted')</script>";
-echo "<script>window.open('index.php?view_store','_self')</script>";
+move_uploaded_file($tmp_image,"blog_images/$blog_image");
+$insert_blog = "insert into blog (blog_title,blog_image,blog_desc,blog_button,blog_url) values ('$blog_title','$blog_image','$blog_desc','$blog_button','$blog_url')";
+$run_blog = mysqli_query($con,$insert_blog);
+if($run_blog){
+echo "<script>alert('New blog Column Has Been Inserted')</script>";
+echo "<script>window.open('index.php?view_blog','_self')</script>";
 }
 }
 }
