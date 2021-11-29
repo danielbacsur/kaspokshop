a2b80ab6702603852118e52600a991ea175f4ee9
danielbacsur <contact@danielbacsur.com>
2021-11-29 10:40:39 +0100

    ac4

--- account.php
+++ account.php
@@ -74,11 +74,14 @@
                                         <form action="" method="post" ><!--form Starts -->
                                             <div class="row row-cols-1 row-cols-md-2">
                                                 <div class="col margin-4-rem-bottom sm-margin-25px-bottom" ><!-- form-group Starts -->
-                                                    <input type="text" class="medium-input bg-white margin-25px-bottom required" name="c_email" required >
+                                                    <input type="text" class="medium-input bg-white margin-25px-bottom required" name="c_email" placeholder="A Neved">
                                                 </div><!-- form-group Ends -->
                                                 <div class="col margin-4-rem-bottom sm-margin-10px-bottom" ><!-- form-group Starts -->
-                                                    <input type="password" class="medium-input bg-white margin-25px-bottom required" name="c_pass" required >
+                                                    <input type="password" class="medium-input bg-white margin-25px-bottom required" name="c_pass" placeholder="A Neved">
                                                 </div><!-- form-group Ends -->
+                                                <div class="col text-start sm-margin-30px-bottom">
+                                                    <span for="terms_condition" class="text-small d-inline-block align-top w-85">Elfelejtetted a jelszavad?<a href="" target="_blank" class="text-decoration-underline text-extra-dark-gray">Jelszó helyreállítása</a>.</span>
+                                                </div>
                                                 <div class="col text-center text-md-end" ><!-- text-center Starts -->
                                                     <button name="login" value="Login" class="btn btn-medium btn-rounded btn-transparent-dark-gray mb-0 submit" > LOG</button>
                                                 </div><!-- text-center Ends -->
