c43cfd4cbcf6f1796bb872f96e2ad9886ae19e74
danielbacsur <contact@danielbacsur.com>
2021-11-29 12:39:09 +0100

    Update account.php

--- account.php
+++ account.php
@@ -88,33 +88,6 @@
                                             </div>
                                         </form><!--form Ends -->
                                         <!-- end contact form -->
-                                        <?php
-                                            if(isset($_POST['login'])){
-                                                $customer_email = $_POST['c_email'];
-                                                $customer_pass = $_POST['c_pass'];
-                                                $select_customer = "select * from customers where customer_email='$customer_email' AND customer_pass='$customer_pass'";
-                                                $run_customer = mysqli_query($con,$select_customer);
-                                                $get_ip = getRealUserIp();
-                                                $check_customer = mysqli_num_rows($run_customer);
-                                                $select_cart = "select * from cart where ip_add='$get_ip'";
-                                                $run_cart = mysqli_query($con,$select_cart);
-                                                $check_cart = mysqli_num_rows($run_cart);
-                                                if($check_customer==0){
-                                                    echo "<script>alert('Helytelen email cím vagy jelszó!')</script>";
-                                                    exit();
-                                                }
-                                                if($check_customer==1 AND $check_cart==0){
-                                                    $_SESSION['customer_email']=$customer_email;
-                                                    echo "<script>alert('Sikeresen bejelentkeztél!')</script>";
-                                                    echo "<script>window.open('account.php','_self')</script>";
-                                                }
-                                                else {
-                                                    $_SESSION['customer_email']=$customer_email;
-                                                    echo "<script>alert('Sikeresen bejelentkeztél!')</script>";
-                                                    echo "<script>window.open('cart.php','_self')</script>";
-                                                } 
-                                            }
-                                        ?>
                                     </div>
                                 </div>
                             </div>
@@ -289,6 +262,34 @@
 		?>
     </body>
 </html>
+
+<?php
+                                            if(isset($_POST['login'])){
+                                                $customer_email = $_POST['c_email'];
+                                                $customer_pass = $_POST['c_pass'];
+                                                $select_customer = "select * from customers where customer_email='$customer_email' AND customer_pass='$customer_pass'";
+                                                $run_customer = mysqli_query($con,$select_customer);
+                                                $get_ip = getRealUserIp();
+                                                $check_customer = mysqli_num_rows($run_customer);
+                                                $select_cart = "select * from cart where ip_add='$get_ip'";
+                                                $run_cart = mysqli_query($con,$select_cart);
+                                                $check_cart = mysqli_num_rows($run_cart);
+                                                if($check_customer==0){
+                                                    echo "<script>alert('Helytelen email cím vagy jelszó!')</script>";
+                                                    exit();
+                                                }
+                                                if($check_customer==1 AND $check_cart==0){
+                                                    $_SESSION['customer_email']=$customer_email;
+                                                    echo "<script>alert('Sikeresen bejelentkeztél!')</script>";
+                                                    echo "<script>window.open('account.php','_self')</script>";
+                                                }
+                                                else {
+                                                    $_SESSION['customer_email']=$customer_email;
+                                                    echo "<script>alert('Sikeresen bejelentkeztél!')</script>";
+                                                    echo "<script>window.open('cart.php','_self')</script>";
+                                                } 
+                                            }
+                                        ?>
 
 <script>
 $(document).ready(function(){
