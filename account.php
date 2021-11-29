f32765e9efb75d2be4af1275be5af2829b51aa25
danielbacsur <contact@danielbacsur.com>
2021-11-29 12:56:54 +0100

    Update account.php

--- account.php
+++ account.php
@@ -91,38 +91,38 @@
                                         <!-- end contact form -->
                                         <?php
                                             if(isset($_POST['login'])){
-                                                echo "a";
+                                                echo "<script>console.log('aa');</script>";
                                                 $customer_email = $_POST['c_email'];
                                                 $customer_pass = $_POST['c_pass'];
                                                 $select_customer = "select * from customers where customer_email='$customer_email' AND customer_pass='$customer_pass'";
                                                 $run_customer = mysqli_query($con,$select_customer);
                                                 $get_ip = getRealUserIp();
-                                                echo "ab";
+                                                echo "<script>console.log('ab');</script>";
                                                 $check_customer = mysqli_num_rows($run_customer);
                                                 $select_cart = "select * from cart where ip_add='$get_ip'";
                                                 $run_cart = mysqli_query($con,$select_cart);
                                                 $check_cart = mysqli_num_rows($run_cart);
-                                                echo "ac";
+                                                echo "<script>console.log('ac');</script>";
                                                 if($check_customer==0){
                                                     echo "<script>alert('password or email is wrong')</script>";
                                                     exit();
-                                                    echo "ad";
+                                                    echo "<script>console.log('ad');</script>";
                                                 }
                                                 if($check_customer==1 AND $check_cart==0){
                                                     $_SESSION['customer_email']=$customer_email;
                                                     echo "<script>alert('You are Logged In')</script>";
                                                     echo "<script>window.open('account.php','_self')</script>";
-                                                    echo "ae";
+                                                    echo "<script>console.log('ae');</script>";
                                                 }
                                                 else {
                                                     $_SESSION['customer_email']=$customer_email;
                                                     echo "<script>alert('You are Logged In')</script>";
                                                     echo "<script>window.open('cart.php','_self')</script>";
-                                                    echo "af";
+                                                    echo "<script>console.log('af');</script>";
                                                 } 
-                                                echo "az";
+                                                echo "<script>console.log('az');</script>";
                                             }
-                                            echo "ax";
+                                            echo "<script>console.log('ax');</script>";
                                         ?>
                                     </div>
                                 </div>
