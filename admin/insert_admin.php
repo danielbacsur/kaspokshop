3a1d29dbd4447d3afffbf54c4c08b84d40f9b692
danielbacsur <contact@danielbacsur.com>
2021-12-05 13:21:02 +0100

    Update insert_admin.php

--- admin/insert_admin.php
+++ admin/insert_admin.php
@@ -42,33 +42,9 @@ else {
 </div><!-- col-md-6 Ends -->
 </div><!-- form-group Ends -->
 <div class="form-group"><!-- form-group Starts -->
-<label class="col-md-3 control-label">User Country: </label>
-<div class="col-md-6"><!-- col-md-6 Starts -->
-<input type="text" name="admin_country" class="form-control" required>
-</div><!-- col-md-6 Ends -->
-</div><!-- form-group Ends -->
-<div class="form-group"><!-- form-group Starts -->
-<label class="col-md-3 control-label">User Job: </label>
-<div class="col-md-6"><!-- col-md-6 Starts -->
-<input type="text" name="admin_job" class="form-control" required>
-</div><!-- col-md-6 Ends -->
-</div><!-- form-group Ends -->
-<div class="form-group"><!-- form-group Starts -->
 <label class="col-md-3 control-label">User Contact: </label>
 <div class="col-md-6"><!-- col-md-6 Starts -->
 <input type="text" name="admin_contact" class="form-control" required>
-</div><!-- col-md-6 Ends -->
-</div><!-- form-group Ends -->
-<div class="form-group"><!-- form-group Starts -->
-<label class="col-md-3 control-label">User About: </label>
-<div class="col-md-6"><!-- col-md-6 Starts -->
-<textarea name="admin_about" class="form-control" rows="3"> </textarea>
-</div><!-- col-md-6 Ends -->
-</div><!-- form-group Ends -->
-<div class="form-group"><!-- form-group Starts -->
-<label class="col-md-3 control-label">User Image: </label>
-<div class="col-md-6"><!-- col-md-6 Starts -->
-<input type="file" name="admin_image" class="form-control" required>
 </div><!-- col-md-6 Ends -->
 </div><!-- form-group Ends -->
 <div class="form-group"><!-- form-group Starts -->
