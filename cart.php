b32a245ab7bb0e6d79dd2abcddcec5a1929ac768
danielbacsur <contact@danielbacsur.com>
2021-12-05 12:26:12 +0100

    Update cart.php

--- cart.php
+++ cart.php
@@ -71,12 +71,12 @@
                                                         $get_products = "select * from products where product_id='$pro_id'";
                                                         $run_products = mysqli_query($con,$get_products);
                                                         while($row_products = mysqli_fetch_array($run_products)){
-                                                            $select_color1 = "select * from cart where id=$pro_color";
+                                                            $select_color1 = "select * from colors where id=$pro_color";
                                                             $run_color1 = mysqli_query($con,$select_colors1);
                                                             $row_color1 = mysqli_fetch_array($run_color1);
                                                             $color_name1 = $row_color1['name'];
 
-                                                            $select_color2 = "select * from cart where id=$pro_size";
+                                                            $select_color2 = "select * from colors where id=$pro_size";
                                                             $run_color2 = mysqli_query($con,$select_colors2);
                                                             $row_color2 = mysqli_fetch_array($run_color2);
                                                             $color_name2 = $row_color2['name'];
