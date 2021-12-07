0212b13a2f0a4a1510adc24d1cb3f7113cbd669b
danielbacsur <contact@danielbacsur.com>
2021-12-07 18:40:10 +0100

    Update confirm_order.php

--- admin/confirm_order.php
+++ admin/confirm_order.php
@@ -5,13 +5,13 @@ if(!isset($_SESSION['admin_email'])){
     echo "<script>console.log('alma')</script>";
     if(isset($_GET['confirm_order'])){
         $delete_id = $_GET['confirm_order'];
-        $delete_order = "update `orders` set `order_status` = 'confirmed' where order_id` = '$delete_id'";
+        $delete_order = "update 'orders' set 'order_status' = 'confirmed' where order_id = '$delete_id'";
         $run_delete = mysqli_query($con,$delete_order);
         if($run_delete){
             echo "<script>alert('Rendelés Megerősitése sikeres!')</script>";
             echo "<script>window.open('index.php?view_orders','_self')</script>";
         }
-        echo "<script>console.log('$run_delete')</script>";
+        echo "<script>console.log('$delete_id')</script>";
 
     }
 } ?>
